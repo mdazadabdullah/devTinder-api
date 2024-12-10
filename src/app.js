e4f173e36e960f2fb5cc4ba6cf819cc3bab382dd
mdazadabdullah <jobs4angular@gmail.com>
2024-12-10 09:44:22 -0800

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -1,26 +1,29 @@
 const express = require("express");
-
 const app = express();
 
-const { adminAuth, userAuth } = require("./middlewares/auth");
-
-
-app.post("/user/login", (req, res) => {
-  res.send("User logged in successfully!");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    // Log your error
+    res.status(500).send("something went wrong");
+  }
 });
 
-app.get("/user/data", userAuth, (req, res) => {
+app.get("/getUserData", (req, res) => {
+  //try {
+  // Logic of DB call and get user data
+
+  throw new Error("dvbzhjf");
   res.send("User Data Sent");
+  //   } catch (err) {
+  //     res.status(500).send("Some Error contact support team");
+  //   }
 });
 
-app.use("/admin", adminAuth);
-
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All Data Sent");
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("Deleted a user");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    // Log your error
+    res.status(500).send("something went wrong");
+  }
 });
 
 app.listen(7777, () => {
