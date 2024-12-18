92209289808d4bf9343faedc98a1be394d7c3a5f
mdazadabdullah <jobs4angular@gmail.com>
2024-12-18 14:24:04 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -6,14 +6,47 @@ const User = require("./models/user");
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  // Creating a new instance of the User model
+  //   Creating a new instance of the User model
   const user = new User(req.body);
-
+  console.log(user);
   try {
     await user.save();
     res.send("User Added successfully!");
   } catch (err) {
     res.status(400).send("Error saving the user:" + err.message);
+  }
+});
+
+// Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+
+    // const users = await User.find({ emailId: userEmail });
+    // if (users.length === 0) {
+    //   res.status(404).send("User not found");
+    // } else {
+    //   res.send(users);
+    // }
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
+  }
+});
+
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
   }
 });
 
